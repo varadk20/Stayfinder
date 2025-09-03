f376e47ce0617f46cde93575711895e370204ffd
Sanika <sanikaambrale20@gmail.com>
2025-09-03 19:08:50 +0530

    Signup/Login with role storage

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './assets/pages/Login.jsx';
 import Signup from './assets/pages/Signup.jsx';
-import Home from './assets/pages/Home.jsx';
+//import Home from './assets/pages/Home.jsx';
+import HostDashboard from "./assets/pages/HostDashboard.jsx";
+import GuestDashboard from "./assets/pages/GuestDashboard.jsx";
 import Details from './assets/pages/Details.jsx'
 import Map from './assets/components/Map.jsx'
 import Date from './assets/components/Datepicker.jsx'
@@ -12,6 +14,8 @@ import MyListings from './assets/pages/MyListings.jsx'
 import EditListing from './assets/pages/EditListing';
 import Status from './assets/components/Status.jsx'
 import 'leaflet/dist/leaflet.css';
+import Analytics from "./assets/pages/Analytics";
+import Contact from "./assets/pages/Contact";
 
 
 
@@ -21,7 +25,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/hostDashboard" element={<HostDashboard />} />
+        <Route path="/guestDashboard" element={<GuestDashboard />} />
         <Route path="/map" element={<Map />} />
         <Route path="/details/:id" element={<Details />} />
         <Route path="/date" element={<Date/>} />
@@ -29,6 +34,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/mylistings" element={<MyListings/>} />
         <Route path="/edit-listing/:id" element={<EditListing />} />
         <Route path="/status" element={<Status />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/contact" element={<Contact />} />
 
       </Routes>
     </BrowserRouter>
